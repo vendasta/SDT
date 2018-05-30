c9c7d42baad9c54b5a110b268a2d5fb8b8d9a288
Braden Bassingthwaite <bbassingthwaite@vendasta.com>
2018-05-30 14:29:59 -0600

    - steppin

--- server/main.go
+++ server/main.go
@@ -3,43 +3,17 @@ package main
 import (
 	"net/http"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-	"github.com/vendasta/SDT/internal"
 )
 
 func main() {
-	app := sdt.NewApplication()
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
-		if app.Ready() {
-			err := app.Ping()
-			if err != nil {
-				http.Error(writer, err.Error(), http.StatusInternalServerError)
-				return
-			} else {
-				fmt.Fprint(writer, "OK")
-			}
-		} else {
-			http.Error(writer, "Not Ready", http.StatusBadRequest)
-			return
-		}
-	})
-
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(writer, "%d", Fibonacci(35))
 	})
 
-	// subscribe to SIGINT signals
-	stopChan := make(chan os.Signal)
-	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
-
 	srv := &http.Server{Addr: ":11000", Handler: mux}
-
 	srv.ListenAndServe()
-
 }
 
 func Fibonacci(n uint64) uint64 {
